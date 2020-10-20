4d835ccf21852e198edb9846df5b474bcde27cb7
shivarad <shiva.rrad@gmail.com>
2020-10-20 22:23:57 +0330

    add sign up with email and password

--- ec-website/src/App.js
+++ ec-website/src/App.js
@@ -29,11 +29,13 @@ class App extends React.Component {
               ...snapShot.data()
             }
           });
+          // console.log(this.state);
+
         });
       }
       else
       { this.setState({ currentUser: userAuth });
-    }
+    } 
     });
   }
   
